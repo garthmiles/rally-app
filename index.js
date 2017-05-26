299ca90175a4e8f8b57bbe685a2d752cf27f27c4
GarthMiles <gmiles@denvergrp.com>
2017-05-26 13:38:10 -0400

    importing 'config' & 'rally'  module

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 #! /usr/bin/env node
 process.env.NODE_CONFIG_DIR = process.env.NODE_CONFIG_DIR ? process.env.NODE_CONFIG_DIR : __dirname + '/config/';
+
+import config from ('config');
+import rally from 'rally';
+
+
+let restAPI = rally({
+
+})
